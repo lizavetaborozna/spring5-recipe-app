c92cda689bae2300354a1ef6d7d9364f83de7094
Lizaveta Barazna <lizavetaborozna@gmail.com>
2022-11-17 22:53:56 +0300

    Add ingredient entity and relationship with recipe

--- src/main/java/guru/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -15,6 +16,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -82,6 +86,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
