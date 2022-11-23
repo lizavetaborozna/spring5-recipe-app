ca34d511dbad4ca2da208da906c4d323a29e79e2
Lizaveta Barazna <lizavetaborozna@gmail.com>
2022-11-23 23:30:36 +0300

    Add lombok dependency. Refactor with lombok

--- src/main/java/guru/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/spring5recipeapp/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package guru.spring5recipeapp.controllers;
 
 import guru.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -15,6 +17,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Get index page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
--- src/main/java/guru/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.spring5recipeapp.services;
 
 import guru.spring5recipeapp.domain.Recipe;
 import guru.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am here");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
